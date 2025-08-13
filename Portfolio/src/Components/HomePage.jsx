e82b951eebea9196e5f6325a6bf3fa36e8001a37
HarshiitaaRajoriiaa <harshitarajoria02@gmail.com>
2025-08-14 00:25:46 +0530

    commit Final Changes

--- Portfolio/src/Components/HomePage.jsx
+++ Portfolio/src/Components/HomePage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Header from "../Components/Header";
 import Footer from "./Footer";
-import About from "../Components/About"
+import About from "../Components/About";
 import Project from "./Projects/Project";
 import Contact from "./Contact";
 import Skill from "../Components/Skills";
 import Ach from "../Components/Ach";
 import LandingPage from "../Components/Landingpage";
+import Wp from "../Components/Wp"
 
 export default function HomePage() {
   return (
     <div>
       <Header />
+      <Wp />
       <LandingPage />
       <About />
       <Project />
